b413d1caedfa066473739aa635c7a35eca0a0eab
falabellamichael <falabellamichael@gmail.com>
2026-05-08 03:09:25 -0400

    Remove backend from public repo

--- backend/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-router.post('/ban', userController.banUser);
-
-module.exports = router;
