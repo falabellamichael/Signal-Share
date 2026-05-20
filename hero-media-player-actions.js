7791fe67339305ef605e06c6d0af25ada7b5000a
falabellamichael <falabellamichael@gmail.com>
2026-05-20 19:22:59 -0400

    update

---
+++ hero-media-player-actions.js
@@ -0,0 +1,10 @@
+export {
+  debounceAction,
+  handleOpenMediaAction,
+  handleOpenPhoneAction,
+  handlePlayPauseAction,
+  handlePreviousAction,
+  handleNextAction,
+  handleVolumeAction,
+  handleRefreshAction
+} from "./hero-media-player-actions-temp-old.js";
